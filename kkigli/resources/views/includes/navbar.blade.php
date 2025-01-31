57662157e1de65ae8ab6e3b475a3523050381086
hayaepon <nfshaya4@gmail.com>
2025-02-01 00:12:05 +0700

    update navbar

--- kkigli/resources/views/includes/navbar.blade.php
+++ kkigli/resources/views/includes/navbar.blade.php
@@ -10,11 +10,21 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link active" href="{{ url('/') }}">Beranda</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ url('/layanan') }}">Layanan</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">Tentang</a></li>
-                <li class="nav-item"><a class="nav-link" href="https://kb-gli.my.id/blog/">Blog</a></li>
-                <li class="nav-item"><a class="btn btn-primary" href=https://wa.me/62895332360440?text=Hallo%20Admin%20Gli.Net,%20Saya%20ingin%20berlangganan%20paket%20internet,%20mohon%20follow%20up%20untuk%20langkah%20selanjutnya%20ya,%20Terimakasih!">Berlangganan Sekarang</a></li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ url('/') }}">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('layanan') ? 'active' : '' }}" href="{{ url('/layanan') }}">Layanan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('about') ? 'active' : '' }}" href="{{ url('/about') }}">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('blog*') ? 'active' : '' }}" href="https://kb-gli.my.id/blog/">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="https://wa.me/62895332360440?text=Hallo%20Admin%20Gli.Net,%20Saya%20ingin%20berlangganan%20paket%20internet,%20mohon%20follow%20up%20untuk%20langkah%20selanjutnya%20ya,%20Terimakasih!">Berlangganan Sekarang</a>
+                </li>
             </ul>
         </div>
     </div>
